797d1f37f4e368f29bdcceeafdfc112d325ceb5f
mpacdia <146739066+mpacdia@users.noreply.github.com>
2024-01-11 14:52:15 +0000

    arreglado ejercicio 4

--- Assets/Scripts/EjercicioCuatro/CreacionObstaculos.cs
+++ Assets/Scripts/EjercicioCuatro/CreacionObstaculos.cs
@@ -30,20 +30,19 @@ public class CreacionObstaculos : MonoBehaviour
                 toGo = rayHit.point;
                 agent.destination = toGo;
             }
-
-            if (Input.GetMouseButtonDown(0))
+        }
+        if (Input.GetMouseButtonDown(0))
             {
                 /* TODO: Lanzar un rayo, e instanciar un obstáculo en el punto donde se golpee */
                 /* Hay que reconstruir la superficie, busca un método que se encargue de ello */
                 if (Physics.Raycast(cameraRay, out rayHit, Mathf.Infinity))
                 {
-                    Object.Instantiate(obstaculo, rayHit.point, obstaculo.transform.localRotation);
+                    Object.Instantiate(obstaculo, new Vector3(rayHit.point.x, rayHit.point.y + 1, rayHit.point.z), obstaculo.transform.localRotation);
                     superficie.BuildNavMesh();
                     agent.ResetPath();
                     agent.destination = toGo;
                 }
             }
-        }
 
     }
 }
